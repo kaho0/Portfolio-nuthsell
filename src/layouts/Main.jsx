d5b33a79647784cfcac707e7de9a11dcd0bb730d
chistym17 <mchisty31@gmail.com>
2024-01-06 14:35:37 +0600

    about me section added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import AboutSection from '../components/AboutSection'
 const Main = () => {
   return (
     <div>
@@ -7,6 +8,7 @@ const Main = () => {
       <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
+     <AboutSection></AboutSection>
     </div>
   )
 }
